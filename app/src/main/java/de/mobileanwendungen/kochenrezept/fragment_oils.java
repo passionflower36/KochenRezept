c8ad7e70d98d74f86792f197b059136dfbb20225
Kuebra Tokuc <kuebra.tokuc@zeit-consulting.de>
2019-06-02 18:29:18 +0200

    Mal gucken, ob er updated

---
+++ app/src/main/java/de/mobileanwendungen/kochenrezept/fragment_oils.java
@@ -0,0 +1,22 @@
+package de.mobileanwendungen.kochenrezept;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class fragment_oils extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_oils, container, false);
+    }
+}
---
+++ app/src/main/java/de/mobileanwendungen/kochenrezept/fragment_proteins.java
@@ -0,0 +1,21 @@
+package de.mobileanwendungen.kochenrezept;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class fragment_proteins extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_proteins, container, false);
+    }
+
+}
+
+

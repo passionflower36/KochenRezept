98e12037c98c3e4f346ffef9407161cb6fb08351
passionflower36 <passionflower@web.de>
2019-06-14 11:39:23 +0200

    Merge branch 'kueb'
    # Conflicts:
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/CarbsFragment.java
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/RezeptFragment.java
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/fragment_drinks.java
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/fragment_junks.java
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/fragment_oils.java
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/fragment_proteins.java
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/fragment_vegs.java
    #	app/src/main/java/de/mobileanwendungen/kochenrezept/profil2.java
    #	app/src/main/res/drawable/panda.png
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/fragment_carbs.xml
    #	app/src/main/res/layout/fragment_drinks.xml
    #	app/src/main/res/layout/fragment_junks.xml
    #	app/src/main/res/layout/fragment_oils.xml
    #	app/src/main/res/layout/fragment_profil2.xml
    #	app/src/main/res/layout/fragment_proteins.xml
    #	app/src/main/res/layout/fragment_rezept.xml
    #	app/src/main/res/layout/fragment_vegs.xml
    #	app/src/main/res/layout/fragment_wissen.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/de/mobileanwendungen/kochenrezept/rezept_obstsalat.java
+++ app/src/main/java/de/mobileanwendungen/kochenrezept/rezept_obstsalat.java
@@ -1,16 +1,19 @@
 package de.mobileanwendungen.kochenrezept;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class rezept_obstsalat extends Fragment {
 
     ImageView headerimg;
+    Button zumSpiel;
 
 
     public ImageView getHeaderimg() {
@@ -29,9 +32,23 @@ public class rezept_obstsalat extends Fragment {
 
        // headerimg = (ImageView) rootView.findViewById(R.id.img_recipe);
 
+        zumSpiel = rootView.findViewById(R.id.zumSpiel);
 
+        //--------Button zumSpiel-------------------------
+        zumSpiel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSpielen();
+            }
+        });
+        //-----------------------------------------------
 
         return rootView;
+    }
+    //------oeffnet das Spiel-------------------
+    public void openSpielen(){
+        Intent intent = new Intent(getActivity(), spielen.class);
+        startActivity(intent);
     }
 
 }
